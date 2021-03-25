10558105a4b05ebec6cc1a060cdc0873629423a9
Stéphane ISNARD <stephane.isnard@univ-tlse2.fr>
2021-03-25 10:12:40 +0100

    upd app.go

--- app/app.go
+++ app/app.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	const nb_cote = 4
+	const nb_cote = 2
 	const taille = 6
 
 	traceur.PrintCoords()
@@ -14,7 +14,7 @@ func main() {
 	traceur.Down("black")
 
 	var i int
-	for i = 0; i < 2; i++ {
+	for i = 0; i < nb_cote; i++ {
 		traceur.Forward(taille)
 		traceur.Right()
 		traceur.PrintCoords()
