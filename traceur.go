977b903885c643ce951f0360de9f79d496c78407
jaco60 <jacoboni@univ-tlse2.fr>
2021-04-18 10:07:46 +0200

    change global var to private

--- traceur.go
+++ traceur.go
@@ -3,12 +3,12 @@ package traceur
 import "fmt"
 
 // Angle pour revenir au Nord (en degrés)
-var AngleToReturnToNorth int
+var angleToReturnToNorth int
 
 // Init initialise l'environnement du robot
 func Init() {
 	fmt.Println("draw mode")
-	AngleToReturnToNorth = 0
+	angleToReturnToNorth = 0
 }
 
 // Forward avance le stylet de step pas dans la direction courante
@@ -22,13 +22,13 @@ func Step() { fmt.Println("forward 1") }
 // Right tourne le stylet de 90° vers la droite
 func Right() {
 	fmt.Println("right")
-	AngleToReturnToNorth -= 90
+	angleToReturnToNorth -= 90
 }
 
 // Left tourne le stylet de 90° vers la gauche
 func Left() {
 	fmt.Println("left")
-	AngleToReturnToNorth += 90
+	angleToReturnToNorth += 90
 }
 
 // Say affiche une bulle contenant le message mess
@@ -46,17 +46,17 @@ func Color(col string) { fmt.Printf("color %s\n", col) }
 // Pivote tourne le stylet de angle degrés vers la droite
 func Pivote(angle int) {
 	fmt.Printf("right %d\n", angle)
-	AngleToReturnToNorth -= angle
+	angleToReturnToNorth -= angle
 }
 
 // North dirige le stylet vers le nord
 func North() {
-	if AngleToReturnToNorth < 0 {
-		fmt.Printf("left %d\n", -AngleToReturnToNorth)
+	if angleToReturnToNorth < 0 {
+		fmt.Printf("left %d\n", -angleToReturnToNorth)
 	} else {
-		fmt.Printf("right %d\n", AngleToReturnToNorth)
+		fmt.Printf("right %d\n", angleToReturnToNorth)
 	}
-	AngleToReturnToNorth = 0
+	angleToReturnToNorth = 0
 }
 
 // South dirige le stylet vers le sud
