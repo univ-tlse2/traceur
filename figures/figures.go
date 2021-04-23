8f67cabb7c202ecd123bec609fe09ecd38ca3323
jaco60 <jacoboni@univ-tlse2.fr>
2021-04-23 14:05:36 +0200

    Remove the North initial and final headings

--- figures/figures.go
+++ figures/figures.go
@@ -4,7 +4,6 @@ import . "github.com/univ-tlse2/traceur"
 
 // DrawRect dessine un rectangle de hauteur x largeur pas avec un contour de la couleur indiquée.
 // Son coin inférieur gauche est à la position initiale du stylet.
-// Après le tracé, le stylet est orienté vers le nord.
 func DrawRect(hauteur, largeur float64, couleur string) {
 	//North()
 	Color(couleur)
@@ -15,26 +14,21 @@ func DrawRect(hauteur, largeur float64, couleur string) {
 		Forward(largeur)
 		Right()
 	}
-	//North()
 }
 
 // DrawSquare dessine un carré de nbPas de côté avec un contour de la couleur indiquée.
 // Son coin inférieur gauche est à la position initiale du stylet.
-// Après le tracé, le stylet est orienté vers le nord.
 func DrawSquare(nbPas float64, couleur string) {
 	DrawRect(nbPas, nbPas, couleur)
 }
 
 // DrawTriangle dessine un triangle équilatéral avec des côtés de nbPas et un contour de la couleur indiquée.
 // Son coin inférieur gauche est à la position initiale du stylet.
-// Après le tracé, le stylet est orienté vers le nord.
 func DrawTriangle(nbPas float64, couleur string) {
-	//West()
 	Left()
 	Color(couleur)
 	for nbCotes := 1; nbCotes <= 3; nbCotes++ {
 		Pivote(180 - 60)
 		Forward(nbPas)
 	}
-	//North()
 }
