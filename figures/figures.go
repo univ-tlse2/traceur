e43fee7bdea8cfccb792785886dfca6b066020da
jaco60 <jacoboni@univ-tlse2.fr>
2021-04-23 11:32:12 +0200

    Cosmetic changes

--- figures/figures.go
+++ figures/figures.go
@@ -2,12 +2,12 @@ package figures
 
 import . "github.com/univ-tlse2/traceur"
 
-// DrawRect dessine un rectangle de hauteur x largeur pas et de couleur coul.
+// DrawRect dessine un rectangle de hauteur x largeur pas avec un contour de la couleur indiquée.
 // Son coin inférieur gauche est à la position initiale du stylet.
 // Après le tracé, le stylet est orienté vers le nord.
-func DrawRect(hauteur, largeur float64, coul string) {
+func DrawRect(hauteur, largeur float64, couleur string) {
 	North()
-	Color(coul)
+	Color(couleur)
 	for cpteur := 1; cpteur <= 2; cpteur++ {
 		// Trace un demi-rectangle
 		Forward(hauteur)
@@ -18,19 +18,19 @@ func DrawRect(hauteur, largeur float64, coul string) {
 	North()
 }
 
-// DrawSquare dessine un carré de nbPas de côté et de couleur coul
+// DrawSquare dessine un carré de nbPas de côté avec un contour de la couleur indiquée.
 // Son coin inférieur gauche est à la position initiale du stylet.
 // Après le tracé, le stylet est orienté vers le nord.
-func DrawSquare(nbPas float64, coul string) {
-	DrawRect(nbPas, nbPas, coul)
+func DrawSquare(nbPas float64, couleur string) {
+	DrawRect(nbPas, nbPas, couleur)
 }
 
-// DrawTriangle dessine un triangle équilatéral avec des côtés de nbPas et de couleur coul
+// DrawTriangle dessine un triangle équilatéral avec des côtés de nbPas et un contour de la couleur indiquée.
 // Son coin inférieur gauche est à la position initiale du stylet.
 // Après le tracé, le stylet est orienté vers le nord.
-func DrawTriangle(nbPas float64, coul string) {
+func DrawTriangle(nbPas float64, couleur string) {
 	West()
-	Color(coul)
+	Color(couleur)
 	for nbCotes := 1; nbCotes <= 3; nbCotes++ {
 		Pivote(180 - 60)
 		Forward(nbPas)
